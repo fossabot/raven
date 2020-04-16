26e83fcfe81a9a5056f40bd0e1dac5052eee4e6d
Trevor James Smith <trevor_smith@live.com>
2020-04-16 14:31:14 -0400

    Add grid weights [WIP]

--- raven/utilities/__init__.py
+++ raven/utilities/__init__.py
@@ -1,2 +1,21 @@
-
+from .coords import realization, param
+from .gis import (
+    feature_contains,
+    get_bbox,
+    get_hydrobasins_attributes_wfs,
+    get_hydrobasins_location_wfs,
+    get_raster_wcs,
+    hydrobasins_aggregate,
+    hydrobasins_upstream_ids,
+    select_hybas_domain,
+)
+from .graphs import (
+    hydrograph,
+    mean_annual_hydrograph,
+    spaghetti_annual_hydrograph,
+    ts_fit_graph,
+    ts_graphs,
+)
+from .grid_weights import calc_gridweights
+from .mk_test import check_num_samples, mk_test_calc
 from .regionalization import regionalize, read_gauged_properties, read_gauged_params
